5a6b74d77aa59b74308f913a8316edd6cdceafed
Roman Dolgyi <tosters@gmail.com>
2024-08-22 13:14:54 +0300

    add commit hash to footer

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,11 @@
 <script>
 	import '../app.css';
+
+	const commitHash = import.meta.env.CF_PAGES_COMMIT_SHA || 'Development';
 </script>
 
 <slot></slot>
+
+<div class="absolute bottom-0 right-2 p-2 text-center text-xs text-gray-500">
+	Commit: {commitHash.slice(0, 7)}
+</div>
